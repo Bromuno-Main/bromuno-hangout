7780e8272e557d3b912a53dd3e42f26519df5627
June <105405983+junetheardist@users.noreply.github.com>
2025-02-27 04:48:22 +0100

    styled left bar

--- components/leftbar/LeftBar.tsx
+++ components/leftbar/LeftBar.tsx
@@ -8,56 +8,59 @@ import { Headers } from '../../data';
 function LeftBar() {
   const [navWidth, setNavWidth] = useState(false);
 
-  const handleNavWidth = () =>{
+  const handleNavWidth = () => {
     setTimeout(() => {
-      if(navWidth === false){
+      if (navWidth === false) {
         setNavWidth(true)
-      }else{
+      } else {
         setNavWidth(false)
       }
-    }, 500);
+    }, 100);
   }
-  
 
-  
+
+
 
   return (
-  <>
-    
-    <nav onMouseEnter={handleNavWidth} onMouseLeave={handleNavWidth} className="top-0 bottom-0  h-full z-20 sticky">
-      <div   className={` h-full ${navWidth?"min-w-[156px]":"min-w-[90px]"}  px-1 flex flex-col justify-between items-center  transition-width duration-500`}>
-        <div className="w-full h-fit  justify-items-center content-center">
-          <div  className='flex flex-col w-full py-[21px]'>
-            {
-              Headers.map(({ label, route,image}, index) => {
-                
-              return (
-                
-              <Link key={index} href={route}  className=' h-[55px] w-full justify-items-center content-center'>
-                <div className='relative flex items-center justify-center w-full h-full overflow-hidden gap-4'>
-                  <div className=' overflow-hidden w-[18px] h-[20px]'>
-                    <Image src={image} alt='' width={20} height={20} className='w-full h-full'/>
-                  </div>
-                  <div className={`${navWidth?"opacity-100":"absolute right-[-50px] opacity-0"} transition-opacity duration-200`}>
-                    <p
-                    className={`font-bold text-black text-[18px] leading-[22px]`}>
-                    {label}
-                    </p>
-                  </div>
-                </div>
+    <>
 
-              </Link>
+      <nav onMouseEnter={handleNavWidth} onMouseLeave={handleNavWidth} className="top-0 bottom-0 delay-300 h-full z-20 sticky">
+        <div className={` h-full ${navWidth ? "min-w-[156px]" : "min-w-[90px]"}  px-1 flex flex-col justify-between items-center  transition-width duration-500`}>
+          <div className="w-full h-fit  justify-items-center content-center">
+            <div className='flex flex-col w-full py-[21px]'>
+              {
+                Headers.map(({ label, route, image }, index) => {
 
-              );
+                  return (
 
-              })
-            }
+                    <Link key={index} href={route} className=' h-[55px] outline   w-full justify-center flex  '>
+                      <div className='relative flex items-center hover:bg-black/5   justify-start w-full h-full overflow-hidden gap-4'>
+                        <div className={`${navWidth ? "opacity-0 hidden   " : "  opacity-100"} transition-opacity px-10 duration-100`}>
+                          <Image src={image} alt='' width={20} height={20} className='w-full h-full' />
+                        </div>
+                        <div className={`${navWidth ? "opacity-100 px-8  flex gap-6 hover:bg-black/5 " : "absolute  right-[-50px] opacity-0"} transition-opacity duration-200`}>
+                          <div className=' overflow-hidden w-[18px] h-[20px]'>
+                            <Image src={image} alt='' width={20} height={20} className='w-full h-full' />
+                          </div>
+                          <p
+                            className={`font-bold text-black text-[18px] leading-[22px]`}>
+                            {label}
+                          </p>
+                        </div>
+                      </div>
 
+                    </Link>
+
+                  );
+
+                })
+              }
+
+            </div>
           </div>
+          <Logo />
         </div>
-        <Logo />
-      </div>
-    </nav>
+      </nav>
     </>
   );
 }
