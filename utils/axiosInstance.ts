079ec027c32f4b7c355d76d66c5c117abfb036fc
Mark <officialrrye5@gmail.com>
2025-04-29 09:05:20 +0100

    Updated baseurl

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -10,7 +10,8 @@ export const setUnauthorizedHandler = (handler: () => void) => {
 
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    // baseURL: BASE_URL,
+    baseURL:'https://bromuno-hangout-server.onrender.com/api/v1',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
