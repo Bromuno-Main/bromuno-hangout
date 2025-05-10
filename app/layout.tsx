c12f9e6a410c8bd50f4e693a361a5c7978f52e98
k1ngniizar <huncleniizar@gmail.com>
2025-05-10 16:16:25 +0100

    fixed ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,73 +1,69 @@
-import type {Metadata} from "next";
-import {Big_Shoulders_Display, Livvic} from "next/font/google";
+import type { Metadata } from "next";
+import { Big_Shoulders_Display, Livvic } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 import LeftBar from "../components/leftbar/LeftBar";
 import FormContextProvider from "../context/formContext";
 import "./globals.css";
 import RightBar from "../components/right-bar/RightBar";
-import {Title} from "../components/title/Title";
-import {StoreProvider} from "../redux/StoreProvider";
+import { Title } from "../components/title/Title";
+import { StoreProvider } from "../redux/StoreProvider";
 import LoadingOverlay from "../components/LoadingOverlay";
 import AppInitializer from "../components/AppInitializer";
 
 const bigShouldersDisplay = Big_Shoulders_Display({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    variable: "--big-shoulders",
-    fallback: ["inter"],
-    preload: true,
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--big-shoulders",
+  fallback: ["inter"],
+  preload: true,
 });
 
 const livvic = Livvic({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-    title: "Bromuno-hangout",
-    description: "Bromuno main",
-    icons: "/fav.svg",
+  title: "Bromuno-hangout",
+  description: "Bromuno main",
+  icons: "/fav.svg",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className={bigShouldersDisplay.className}>
-        <body
-            className={`${livvic.className} ${bigShouldersDisplay.variable} gap-2 flex w-screen lg:px-10 bg-green lg:py-2 px-2 py-1 items-center justify-center h-screen flex-col`}
-        >
-        <NextTopLoader/>
+  return (
+    <html lang="en" className={bigShouldersDisplay.className}>
+      <body
+        className={`${livvic.className} ${bigShouldersDisplay.variable} gap-2 flex w-screen lg:px-10 bg-green lg:py-2 px-2 py-1 items-center justify-center h-screen flex-col`}
+      >
+        <NextTopLoader />
 
         <span className="uppercase text-sm opacity-60 text-white hover:opacity-100 duration-300">
-                    bromuno hangout
-                </span>
+          bromuno hangout
+        </span>
 
-        <main
-            className="relative flex bg-white rounded-2xl w-full h-full max-h-[900px] flex-1 max-w-screen-2xl overflow-clip overflow-y-scroll scrollbar-hide">
-            <StoreProvider>
-                <AppInitializer/>
-                <LoadingOverlay/>
-                <FormContextProvider>
-                    <LeftBar/>
-                    <div
-                        className="h-full relative scrollbar-hide flex flex-col flex-1 px-4 bg-gray-50 overflow-hidden overflow-y-scroll pt-[100px] lg:pt-0">
-                        <Title className="hidden lg:block"/>
-                        <div className="flex-1 py-2">{children}</div>
-                    </div>
-                    
-                    <RightBar/>
-                    
-                    
-                </FormContextProvider>
-            </StoreProvider>
+        <main className="relative flex bg-white rounded-2xl w-full h-full lg:max-h-[900px]  flex-1 max-w-screen-2xl overflow-clip overflow-y-scroll scrollbar-hide">
+          <StoreProvider>
+            <AppInitializer />
+            <LoadingOverlay />
+            <FormContextProvider>
+              <LeftBar />
+              <div className="h-full relative scrollbar-hide flex flex-col flex-1 px-4 bg-gray-50 overflow-hidden overflow-y-scroll pt-[100px] lg:pt-0">
+                <Title className="hidden lg:block" />
+                <div className="flex-1 py-2">{children}</div>
+              </div>
+
+              <RightBar />
+            </FormContextProvider>
+          </StoreProvider>
         </main>
         <span className="text-sm text-white opacity-60 hover:opacity-100 duration-300">
-                    Copyright © 2025 Bromuno technologies. All rights reserved
-                </span>
-        </body>
-        </html>
-    );
+          Copyright © 2025 Bromuno technologies. All rights reserved
+        </span>
+      </body>
+    </html>
+  );
 }
