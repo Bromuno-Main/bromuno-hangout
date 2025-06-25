8c464a86c68bdc961fc0944124a867b80062c038
Mark <officialrrye5@gmail.com>
2025-06-25 15:22:26 +0100

    Add waitlist functionality

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -11,6 +11,7 @@ export const setUnauthorizedHandler = (handler: () => void) => {
 
 const axiosInstance = axios.create({
     // baseURL: BASE_URL,
+
     baseURL:'https://bromuno-hangout-server.onrender.com/api/v1',
     timeout: 40000,
     headers: {
